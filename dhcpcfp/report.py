6dae14433c097964c845b7fc75ef4911d0cd252c
juga0 <juga@riseup.net>
2017-08-17 23:06:21 +0200

    Add possibility to append to the report,
    and remove not needed logging config.

--- dhcpcfp/report.py
+++ dhcpcfp/report.py
@@ -7,16 +7,17 @@ import io
 import logging
 from string import Template
 
-from .conflog import LOGGING
-
-logging.config.dictConfig(LOGGING)
 logger = logging.getLogger('dhcpcfp')
 
 
-def write_md_report(data, content, outfpath):
+def write_md_report(data, content, outfpath, continuous=False):
     logger.debug('data %s', data)
     template = Template(content)
     logger.debug('template %s', template)
     report = template.substitute(**data)
-    with io.open(outfpath, 'wb') as fd:
-        fd.write(report)
+    if continuous:
+        with io.open(outfpath, 'ab') as fd:
+            fd.write(report)
+    else:
+        with io.open(outfpath, 'wb') as fd:
+            fd.write(report)
