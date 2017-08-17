bf9ee34842b7d25711fe730c2e7b789ecdc54403
juga0 <juga@riseup.net>
2017-08-17 22:55:34 +0200

    Rename template and template simple

---
+++ dhcpcfp/template_simple.py
@@ -0,0 +1,7 @@
+content_simple = """
+Found request!!:
+DHCP fingerprint: $dhcp_fp, DHCP vendor: $dhcp_vendor, MAC vendor: $mac_vendor
+Devices/operating systems guessed using MAC: $devices_mac
+Devices/operating systems guessed not using MAC: $devices
+-------------------------------------------------------------------------------
+"""
